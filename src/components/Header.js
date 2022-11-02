b3e6b09285c81b15b8aab16bc6ac644ad52f01c0
Masterzerr <alejerry24@gmail.com>
2022-11-02 21:45:15 +0800

    header

---
+++ src/components/Header.js
@@ -0,0 +1,127 @@
+import React, { useState} from 'react'
+import MyProfile from '../components/myimage.png';
+import './style.css';
+import 'flowbite';
+import { Toast } from 'flowbite-react';
+
+export default function Header() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const copy = () => {
+        navigator.clipboard.writeText("alejerry24@gmail.com");        
+        popup();
+    }
+
+    const popup = event => {
+        setIsActive(current => !current);
+    };
+      
+
+  return (
+    <div>
+        <div className='w-full flex columns-2 h-max'>
+            <div className='px-32 py-32 '>
+                <span className='text-5xl text-white font-light text-left'>Hi! I Am </span>
+                <br/>
+                <span className='text-8xl text-white text-left font-semibold'>Jerry S. Ale </span>
+                <div className='my-10'>
+                    <span className='text-white bg-gradient-to-r from-violet-800 to-indigo-600 rounded-full px-5 py-3 mr-3'>Web Dev</span>
+                    <span className='text-white rounded-full px-5 py-3 bg-gradient-to-r bg-indigo-600 mr-3'>UI/UX</span>
+                    <span className='text-white rounded-full px-5 py-3 bg-gradient-to-r bg-indigo-600'>IT Instructor</span>
+                </div>
+                <div className='pr-32'>
+                    <p className='text-zinc-400 text-left'>
+                        A java programming enthusiast and a web developer primarily focusing on UI and UX design switched into an IT educator
+                        for more than 4 years from a premiere university teaching major subjects such as web development, Java mobile development 
+                        in Android Studio, and Java Programming.
+                    </p>
+                </div>
+                <div className='mt-8 flex align-middle'>
+                    <div className='flex py-3 ml-8 mr-4'>
+                        <p className='text-gray-200'>Connect with me: </p>
+                    </div>
+                    <button className='t-tooltip place-self-center mr-2 hover:cursor-pointer max-w-md' onClick={copy}
+                        data-collapse-toggle="toast">
+                        <img width="34" height="34" src={process.env.PUBLIC_URL + '/icons/gmail.svg'} alt="gmail"/> 
+                        <div className='t-tooltip -ml-8 '>
+                                <div className='hidden t-box bg-slate-200 p-4 text-white rounded-xl absolute mt-2'>
+                                    <div className='columns-2 flex'>
+                                        <div>
+                                            <img  width="70" height="70" src={process.env.PUBLIC_URL + '/icons/profile1.png'} alt="myprofile"/>
+                                        </div>
+                                        <div className='px-3 text-left place-self-center'>
+                                            <h3 className='text-indigo-600 font-semibold'>Connect via Gmail</h3>
+                                            <p className='text-gray-800'>alejerry24@gmail.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>   
+                    </button>
+
+                    <button className='t-tooltip place-self-center mr-2 hover:cursor-pointer max-w-md'
+                        data-collapse-toggle="toast">
+                        <a href='https://www.linkedin.com/in/jerryale/' target="_blank" rel="noreferrer">
+                            <img width="42" height="42" src={process.env.PUBLIC_URL + '/icons/linkedin.svg'} alt="linked in"/>   
+                        </a>
+                        
+                        <div className='t-tooltip -ml-8 '>
+                                <div className='hidden t-box bg-slate-200 p-4 text-white rounded-xl absolute mt-1'>
+                                    <div className='columns-2 flex'>
+                                        <div>
+                                            <img  width="70" height="70" src={process.env.PUBLIC_URL + '/icons/profile1.png'} alt="myprofile"/>
+                                        </div>
+                                        <div className='px-3 text-left place-self-center'>
+                                            <h3 className='text-indigo-600 font-semibold'>Connect via Linked In</h3>
+                                            <p className='text-gray-800'>https://www.linkedin.com/in/jerryale/</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                    </button>
+
+
+                    <button className='t-tooltip place-self-center mr-2 hover:cursor-pointer max-w-md'
+                        data-collapse-toggle="toast">
+                        <a href='https://www.linkedin.com/in/jerryale/' target="_blank" rel="noreferrer">
+                            <img width="34" height="34" src={process.env.PUBLIC_URL + '/icons/github.svg'} alt="GitHub"/>  
+                        </a>
+                        
+                        <div className='t-tooltip -ml-8 '>
+                                <div className='hidden t-box bg-slate-200 p-4 text-white rounded-xl absolute mt-2'>
+                                    <div className='columns-2 flex'>
+                                        <div>
+                                            <img  width="70" height="70" src={process.env.PUBLIC_URL + '/icons/gitprofile.jpg'} alt="myprofile"/>
+                                        </div>
+                                        <div className='px-3 text-left place-self-center'>
+                                            <h3 className='text-indigo-600 font-semibold'>View my Repository</h3>
+                                            <p className='text-gray-800'>https://github.com/masterz3rr</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                    </button>   
+
+                </div>
+            </div>
+
+            <div className='inline-block w-6/12 bg-gradient-to-r from-violet-800 to-indigo-600 '>
+                <img className="-ml-60 " src={MyProfile} alt="myprofile"/>
+            </div>
+        </div>
+
+       
+            <div className={ isActive ? 'inline-block absolute top-5 left-5':'hidden absolute top-5 left-5'}>
+            <Toast>
+                <div className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-blue-500 dark:bg-blue-800 dark:text-blue-200">  
+                    <img width="34" height="34" src={process.env.PUBLIC_URL + '/icons/gmail.svg'} alt="gmail"/> 
+                </div>
+                <div className="ml-3 text-sm font-normal">
+                    Gmail copied to your clipboard.
+                </div>
+            <Toast.Toggle />
+            </Toast>
+        </div>    
+    </div>
+  )
+}
