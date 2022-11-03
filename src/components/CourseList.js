4d608de16f18120c76249234eb7f0dc4de5f99a6
Masterzerr <alejerry24@gmail.com>
2022-11-03 18:42:40 +0800

    additional svg

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -7,63 +7,121 @@ export default function CourseList() {
         <ul className="mb-8 mt-3 space-y-3 text-left text-gray-300 dark:text-gray-200">
             <li className="flex items-start space-x-2">
                 <svg className="flex w-5 h-5 text-green-500 dark:t CSSext-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                
+   
                 <div className='block'>
                     <p className="font-semibold">Web Development/Web Enterprise</p>
-                    <p className="text-gray-400">Core Tech Stack </p>
-                    <div className='h-12 flex mb-3'>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/html.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/css3.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/javascript.svg'} alt="react logo"/>
-                        <img className='w-12 mt-3 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/php.svg'} alt="react logo"/>
-                    </div>
-                    <p className="text-gray-400 mt-5 mb-2">Libraries and Frameworks</p>
-                    <div className='h-12 flex'>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/laravel.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/react.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/bootstrap.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/tailwind.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/sass.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/mui.svg'} alt="react logo"/>
-                        <img className='w-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/materialize.svg'} alt="react logo"/>
-                    </div>
+                    <div className='grid grid-cols-2 pl-5'>
+                        <div>
+                            <p className="text-gray-400 mb-2">Core Tech Stack </p>
+                            <div className='h-12 flex mb-3'>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/html.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/css3.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/javascript.svg'} alt="react logo"/>
+                                <img className='w-12 mt-3 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/php.svg'} alt="react logo"/>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-400 mb-2">Libraries and Frameworks</p>
+                            <div className='h-12 flex'>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/laravel.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/react.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/bootstrap.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/tailwind.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/sass.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/mui.svg'} alt="react logo"/>
+                                <img className='w-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/materialize.svg'} alt="react logo"/>
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <p className="text-gray-400 mb-2">Applications for UI Design</p>
+                            <div className='h-12 flex'>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/photoshop.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/illustrator.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/xd.svg'} alt="react logo"/>
+                                <img className='h-10 mt-1 w-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/figma.svg'} alt="react logo"/>
+                            </div>
+                        </div>
 
-                    <p className="text-gray-400 mt-5 mb-2">Applications</p>
-                    <div className='h-12 flex'>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/photoshop.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/illustrator.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/xd.svg'} alt="react logo"/>
-                        <img className='h-10 mt-1 w-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/figma.svg'} alt="react logo"/>
-                    </div>
+                        <div>
+                            <p className="text-gray-400 mb-2">Database Management Systems</p>
+                            <div className='h-12 flex'>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/mysql.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/xampp.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/wamp.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/firebase.svg'} alt="react logo"/>
+                                <img className='h-10 pt-2 overflow-x-hidden pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/mongodb.svg'} alt="react logo"/>
+                            </div>
+                        </div>
 
-                    <p className="text-gray-400 mt-5 mb-2">Database Management Systems</p>
-                    <div className='h-12 flex'>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/mysql.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/illustrator.svg'} alt="react logo"/>
-                        <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/xd.svg'} alt="react logo"/>
-                        <img className='h-10 mt-1 w-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/figma.svg'} alt="react logo"/>
+                        <div>
+                            <p className="mt-3 text-gray-400 mb-2">Content Management System Stack</p>
+                            <div className='h-12 flex'>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/wordpress.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/elementor-icon.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/sage.svg'} alt="react logo"/>
+                                <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/acf.png'} alt="react logo"/>
+                           </div>
+                        </div>
+                       
                     </div>
+                    
                 </div>
             </li>
-            <li className="flex items-center space-x-2">
+            <li className="flex mt-10 items-startspace-x-2">
+                <svg className=" mt-5 flex w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <div className='block'>
+                    <p className="mt-5 ml-2 font-semibold">Mobile Application using Google Android Platform</p>
+                    <div className='pl-7'>
+                        <p className="text-gray-400 mb-2">Stack</p>
+                        <div className='h-12 flex mb-3'>
+                            <img className='h-8 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/java.png'} alt="react logo"/>  
+                            <img className='h-8 pointer-events-none mr-5' src={process.env.PUBLIC_URL + '/icons/android.png'} alt="react logo"/>
+                            <img className='h-8 pointer-events-none mr-5' src={process.env.PUBLIC_URL + '/icons/sqlite.svg'} alt="react logo"/>
+                        </div>
+                    </div>
+                    <div className='pl-7'>
+                        <p className="text-gray-400 mb-2">Currently learning React Native and Flutter</p>
+                    </div>
+                    
+                </div>
                 
-                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                <span>Mobile Application using Google Android Platform</span>
+
+                
             </li>
-            <li className="flex items-center space-x-2">
+            <li className="flex items-start space-x-2">
                 
                 <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                <span>Java Programming - OOP<span className="font-semibold text-gray-900 dark:text-white">1 developer</span></span>
+                <div className='block'>
+                    <p className="ml-2 font-semibold">Java Programming - OOP, Data Structures and Algorithm in Java</p>
+                    <div className='pl-7'>
+                        <p className="text-gray-400 mb-2">Stack</p>
+                        <div className='h-12 flex mb-3'>
+                            <img className='h-8 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/java.png'} alt="react logo"/>  
+                            <img className='h-8 pointer-events-none mr-5' src={process.env.PUBLIC_URL + '/icons/netbeans.png'} alt="react logo"/>
+                        </div>
+                    </div>
+                    <div className='pl-7'>
+                        <p className="text-gray-400 mb-2">Currently learning React Native and Flutter</p>
+                    </div>
+                    
+                </div>
+
             </li>
-            <li className="flex items-center space-x-2">
+            <li className="flex items-start space-x-2">
                 
                 <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                <span>Data Structures and Algorithm in Java <span className="font-semibold text-gray-900 dark:text-white">6 months</span></span>
-            </li>
-            <li className="flex items-center space-x-2">
-                
-                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                <span>.Net Framework with MySQL <span className="font-semibold text-gray-900 dark:text-white">6 months</span></span>
+                <div className='block'>
+                    <p className="ml-2 font-semibold">.NET Framework with MySQL</p>
+                    <div className='pl-7'>
+                        <p className="text-gray-400 mb-2">Stack</p>
+                        <div className='h-12 flex mb-3'>
+                            <img className='h-12 pointer-events-none mr-3' src={process.env.PUBLIC_URL + '/icons/net.svg'} alt="react logo"/>  
+                            <img className='h-12 pointer-events-none mr-5' src={process.env.PUBLIC_URL + '/icons/visual studio.svg'} alt="react logo"/>
+                        </div>
+                    </div>
+                    
+                </div>
             </li>
         </ul>
 
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -19,7 +19,11 @@ export default function Timeline() {
                 <p className="mb-4 text-base font-normal text-gray-300 dark:text-gray-400">
                     Teaching core subjects for Information Technology and Computer Science courses of the <a href='https://bisubalilihan.com/' target="_blank" rel="noreferrer" className="text-teal-300">Bohol Island State University - Balilihan Campus.</a>
                 </p>
-                <p className='font-medium text-white'>Subjects Handled</p>
+                <div className='flex items-center align-middle '>
+                    <svg className="w-3 h-3 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                    <p className='ml-2 uppercase font-medium text-white'>Subjects Handled</p>
+                </div>
+                
                 <CourseList/>
                 <a href="https://bisubalilihan.com/about-bisu/campus-officials/" target="_blank" rel="noreferrer" className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mr-2" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>See University Profile</a>
             </li>
